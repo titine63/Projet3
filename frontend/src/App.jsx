3658d1a0f7b5f70da675c597dbd72866a74d9e67
Jesse Vallant <vallantjesse@live.com>
2023-09-13 13:21:22 +0200

    setup tailwind and tailwindform plugin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,39 @@
 function App() {
-
   return (
-    <>  
-      <h1>React App</h1>
-    </>
-  )
+    // Demo tailwindcss voir fichier index.css pour les classes
+    <div>
+      <h1 className="h1">Hello Khaled !</h1>
+      <section className="section">
+        <h2 className="h2">Connectez-vous</h2>
+        <form className="form">
+          <label htmlFor="email" className="label">
+            Email
+          </label>
+          <input
+
+            type="email"
+            id="email"
+            name="email"
+            className="input"
+            placeholder="Email"
+          />
+          <label htmlFor="password" className="label">
+            Mot de passe
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="input"
+            placeholder="Mot de passe"
+          />
+        </form>
+        <button type="submit" className="button">
+          Se connecter
+        </button>
+      </section>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-    theme: {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  // eslint-disable-next-line no-undef
+  plugins: [require("@tailwindcss/forms")],
+};
