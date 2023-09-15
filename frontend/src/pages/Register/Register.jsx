71cb82b02ab9815925d2e50ab5950baf87718f63
khaledmo99 <khaled.mofleh707@gmail.com>
2023-09-15 17:01:38 +0200

    page register login mobile

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,44 +1,57 @@
 /* eslint-disable react/no-unescaped-entities */
+import { AiOutlineUser } from "react-icons/ai";
+import { HiOutlineMail } from "react-icons/hi";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { Link } from "react-router-dom";
+
 export default function Register() {
   return (
     <>
       <section className="section">
-        <h2 className="h2">Connectez-vous</h2>
-        <form className="form">
-          <label htmlFor="pseudo" className="label">
-            pseudo
-          </label>
-          <input
-            type="text"
-            id="pseudo"
-            name="pseudo"
-            className="input"
-            placeholder="Pseudo"
-          />
-          <label htmlFor="email" className="label">
-            Email
-          </label>
+        <div>
+          <h2 className="h2">
+            Inscrivez<span className="text-[#ec5a13]">.</span>
+          </h2>
+          <h2 className="h2">
+            Explorez<span className="text-[#ec5a13]">.</span>
+          </h2>
+          <h2 className="h2">
+            Economisez<span className="text-[#ec5a13]">.</span>
+          </h2>
+        </div>
+        <form className="form relative">
+          <AiOutlineUser className="absolute left-10 top-10 text-xl text-[#5e5e5e]" />
           <input
             type="email"
             id="email"
             name="email"
-            className="input"
+            className="input-auth"
+            placeholder="Pseudo"
+          />
+          <HiOutlineMail className=" absolute left-10 top-[6.8rem] text-xl text-[#5e5e5e]" />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="input-auth"
             placeholder="Email"
           />
-          <label htmlFor="password" className="label">
-            Mot de passe
-          </label>
+          <RiLockPasswordFill className=" absolute bottom-[2.8rem] left-10 text-xl text-[#5e5e5e]" />
           <input
             type="password"
             id="password"
             name="password"
-            className="input"
+            className="input-auth"
             placeholder="Mot de passe"
           />
         </form>
         <button type="submit" className="button">
-          S'inscrire
+          Créer un compte
         </button>
+
+        <Link to="/login" className="text-l mt-5 underline">
+          Vous avez déjà un compte ?
+        </Link>
       </section>
     </>
   );
