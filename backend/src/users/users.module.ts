8838b42a1f9deba4c32d8898420b3609fc625282
titine63 <sblanchonnet@yahoo.fr>
2023-09-21 10:32:31 +0200

    table users created

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,9 +1,12 @@
+import { User } from './user.entity/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService],
-  controllers: [UsersController]
+  controllers: [UsersController],
 })
 export class UsersModule {}
