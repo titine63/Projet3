c4e6bd5183e8dab51e25968b350d067219c14d14
titine63 <sblanchonnet@yahoo.fr>
2023-09-21 15:32:43 +0200

    modifications on users.service and users.module et users.controller

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+// src/app/users/users.controller.ts
+
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Put,
+  Delete,
+  Param,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './user.entity/user.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private service: UsersService) {}
+
+  @Get()
+  getAll() {
+    return this.service.getUsers();
+  }
+
+  @Get(':id')
+  get(@Param() params) {
+    return this.service.getUser(params.id);
+  }
+
+  @Post()
+  create(@Body() user: User) {
+    return this.service.saveUser(user);
+  }
+
+  @Put()
+  update(@Body() user: User) {
+    return this.service.saveUser(user);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param() params) {
+    this.service.deleteUser(params.id);
+    return;
+  }
+}
