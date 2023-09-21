cce52c78cf0cb59514ea9366301daa738adf6700
khaledmo99 <khaled.mofleh707@gmail.com>
2023-09-21 12:32:54 +0200

    buy-page-home

--- frontend/src/pages/Buy/Buy.jsx
+++ frontend/src/pages/Buy/Buy.jsx
@@ -1,11 +1,37 @@
+import { Link } from "react-router-dom";
+import { AiOutlineFilter } from "react-icons/ai";
+
 /* eslint-disable react/no-unescaped-entities */
 export default function Buy() {
   return (
     <main className="main flex flex-col justify-center">
-      <h1 className="h1">Acheter</h1>
-      <h2 className="h2">
-        Trouvez votre bonheur parmi notre catalogue d'articles d'occasion
-      </h2>
+      <div className="buy-page-home">
+        <h2 className="buy-page-home-h2">Filtrer par catégorie</h2>
+        <div className="buy-imgs">
+          <img
+            className="buy-img"
+            src="./../../../public/images/man-image.png"
+            alt="man-img"
+          />
+          <img
+            className="buy-img"
+            src="./../../../public/images/woman-image.png"
+            alt="woman-img"
+          />
+          <img
+            className="buy-img"
+            src="./../../../public/images/kids-image.png"
+            alt="kids-img"
+          />
+        </div>
+        <div className="tendence">
+          <h2 className="buy-page-home-h2">Tendences du moment</h2>
+          <Link to="/buy" className="button-filter relative pl-3 pr-9">
+            Filtres
+            <AiOutlineFilter className="absolute bottom-1 right-1 text-xl text-[#ec5a13] " />
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
