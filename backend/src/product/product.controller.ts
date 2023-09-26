005e2aa24f0373011af172b96523e656bae2b556
khaledmo99 <khaled.mofleh707@gmail.com>
2023-09-26 15:51:10 +0200

    hello

--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -7,9 +7,9 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
-import { ProductService } from './../product.service';
-import { CreateProductDto } from './../dto/create-product.dto';
-import { UpdateProductDto } from './../dto/update-product.dto';
+import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('product')
 export class ProductController {
