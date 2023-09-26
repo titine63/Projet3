9ac7723554e6e43d303d43f028c6cd88d99c0698
Jesse Vallant <vallantjesse@live.com>
2023-09-26 14:09:45 +0200

    fixed some css

--- frontend/src/components/Products/ProductsCard.jsx
+++ frontend/src/components/Products/ProductsCard.jsx
@@ -1,6 +1,11 @@
 /* eslint-disable react/no-unescaped-entities */
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import { HiUserGroup } from "react-icons/hi";
+import { BsTagsFill } from "react-icons/bs";
+import { PiHandHeartFill } from "react-icons/pi";
+import { GiClothes, GiBodyHeight } from "react-icons/gi";
+import { IoIosColorPalette } from "react-icons/io";
 
 const ProductCard = ({ product }) => {
   const {
@@ -17,76 +22,95 @@ const ProductCard = ({ product }) => {
   } = product;
 
   return (
-    <main className="relative">
-      <div className="hidden xl:fixed xl:left-[5%] xl:top-28 xl:block xl:w-[32%]">
+    <section className="relative">
+      <div className="hidden xl:fixed xl:bottom-[45%] xl:left-[5%] xl:block xl:w-[32%] xl:translate-y-1/2">
         <img src={image} alt={title} className="w-full" />
       </div>
-      <div className="product-card top-8 my-6 xl:absolute xl:right-[15%] xl:w-[37%] ">
-        <h1 className="h1">{title}</h1>
-        <img src={image} alt={title} className="product-imgs xl:hidden" />
+      <div className="product-card">
+        <h1 className="h1 md:mb-6">{title}</h1>
+        <img
+          src={image}
+          alt={title}
+          className="sm:w- m-auto w-[60%] xl:left-0 xl:hidden"
+        />
         <p className="product-price">{price} €</p>
-        <div className="product-details xl:gap-6">
+        <div className="product-details">
           <Link to="/favorites">
             <p className="btn-fav">Ajouter aux favoris</p>
           </Link>
-          <p className="titles mt-7">Description</p>
-
-          <p className="product-description">{description}</p>
-          <p className="titles">Critères</p>
-          <div className="product-Criteria">
-            <p>
-              <span className="criterias">
-                Catégorie <br />
-              </span>
-              {category}
-            </p>
-            <p>
-              <span className="criterias">
-                Type de vêtement <br />
-              </span>
-              {clothing_type}
-            </p>
-            <p>
-              <span className="criterias">
-                Taille <br />
-              </span>
-              {size}
-            </p>
-            <p>
-              <span className="criterias">
-                Marque <br />
-              </span>
-              {brand}
-            </p>
-            <p>
-              <span className="criterias">
-                Couleur <br />
-              </span>
-              {color}
-            </p>
-            <p>
-              <span className="criterias">
-                Etat <br />
-              </span>
-              {state}
-            </p>
+          <div>
+            <h2 className="titles">Description</h2>
+            <p className="md:text-xl">{description}</p>
+          </div>
+          <div>
+            <h2 className="titles">Critères</h2>
+            <div className="product-criteria">
+              <div className="relative">
+                <HiUserGroup className="absolute top-1 text-xl" />
+                <span className="criterias">
+                  Catégorie <br />
+                </span>
+                <span className="pl-8">{category}</span>
+              </div>
+              <div className="relative">
+                <GiBodyHeight className="absolute top-1 text-xl" />
+                <span className="criterias">
+                  Taille <br />
+                </span>
+                <span className="pl-8">{size}</span>
+              </div>
+              <div className="relative">
+                <span className="criterias">
+                  <GiClothes className="absolute top-1 text-xl" />
+                  Type{" "}
+                  <span className="hidden sm:inline-block">
+                    {" "}
+                    de vêtement
+                  </span>{" "}
+                  <br />
+                </span>
+                <span className="pl-8">{clothing_type}</span>
+              </div>
+              <div className="relative">
+                <BsTagsFill className="absolute top-1 text-xl" />
+                <span className="criterias">
+                  Marque <br />
+                </span>
+                <span className="pl-8">{brand}</span>
+              </div>
+              <div className="relative">
+                <IoIosColorPalette className="absolute top-1 text-xl" />
+                <span className="criterias">
+                  Couleur <br />
+                </span>
+                <span className="pl-8">{color}</span>
+              </div>
+              <div className="relative">
+                <PiHandHeartFill className="absolute top-1 text-xl" />
+                <span className="criterias">
+                  Etat <br />
+                </span>
+                <span className="pl-8">{state}</span>
+              </div>
+            </div>
           </div>
           <Link to="/card">
             <p className="buy-btn">Acheter</p>
           </Link>
-
-          <p className="titles">User</p>
-          <div className="user-section">
-            <Link to="/profile">
-              <p className="btn-user">Voir son profil</p>
-            </Link>
-            <Link to="/messages">
-              <p className="btn-user">Envoyer un message</p>
-            </Link>
+          <div>
+            <h2 className="titles">User</h2>
+            <div className="flex justify-between gap-2">
+              <Link to="/profile">
+                <p className="btn-user">Voir le profil</p>
+              </Link>
+              <Link to="/messages">
+                <p className="btn-user">Envoyer un message</p>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </main>
+    </section>
   );
 };
 
