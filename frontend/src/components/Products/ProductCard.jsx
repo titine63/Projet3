ac1191a93c30a13cd93707ef79dea1d0f00ff67f
khaledmo99 <khaled.mofleh707@gmail.com>
2023-10-13 16:30:45 +0200

    wish list COMPLETED git add .

--- frontend/src/components/Products/ProductCard.jsx
+++ frontend/src/components/Products/ProductCard.jsx
@@ -1,24 +1,55 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AiOutlineHeart } from "react-icons/ai";
 import { AiFillHeart } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { GlobalContext } from "./../../contexts/GlobalContextProvider";
 
 export default function ProductCard({ product }) {
+  const { wishlist, setWishlist, showToast } = useContext(GlobalContext);
   const [isFavorite, setIsFavorite] = useState(false);
+
+  console.log("wishlist :>> ", wishlist);
+  const handleAddToWishlist = () => {
+    addToWishlist(product);
+    setIsFavorite(true);
+    showToast("Ajouté !");
+  };
+
+  const addToWishlist = (product) => {
+    setWishlist((prevWishlist) => [...prevWishlist, product]);
+  };
+
+  const handleDeleteFromWishList = () => {
+    deleteFromWishlist(product);
+    setIsFavorite(false);
+    showToast("Supprimé !");
+  };
+
+  const deleteFromWishlist = (product) => {
+    setWishlist((prevWishlist) =>
+      prevWishlist.filter((item) => item.id !== product.id),
+    );
+  };
+
+  useEffect(() => {
+    const productInWishList = wishlist.some((item) => item.id === product.id);
+    setIsFavorite(productInWishList);
+  }, [wishlist, product]);
 
   return (
     <div className="s relative flex flex-col gap-1 rounded-lg border-2 bg-slate-100 p-1 shadow-lg transition duration-300 ease-in-out hover:-translate-y-1 hover:translate-x-1 hover:shadow-xl">
       {!isFavorite && (
         <AiOutlineHeart
           className="absolute right-2 top-1 cursor-pointer text-3xl transition duration-200 ease-in-out hover:scale-110 hover:transform"
-          onClick={() => setIsFavorite(true)}
+          onClick={handleAddToWishlist}
         />
       )}
       {isFavorite && (
         <AiFillHeart
           className="absolute right-2 top-1 cursor-pointer text-3xl text-red-600"
-          onClick={() => setIsFavorite(false)}
+          onClick={handleDeleteFromWishList}
         />
       )}
 
--- frontend/src/pages/Favorites/Favorites.jsx
+++ frontend/src/pages/Favorites/Favorites.jsx
@@ -1,9 +1,22 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/no-unescaped-entities */
+
+import { useContext } from "react";
+import { GlobalContext } from "./../../contexts/GlobalContextProvider";
+import ProductCard from "./../../components/Products/ProductCard";
+
 export default function Favorites() {
+  const { wishlist} = useContext(GlobalContext);
+
   return (
     <main className="main flex flex-col justify-center">
-      <h1 className="h1">Liste d'envie</h1>
-      <h2 className="h2">Retrouvez vos articles favoris ici</h2>
+      <h2 className="h1 my-8">Ma liste d'envies</h2>
+
+      <div className="grid grid-cols-1 gap-4 border p-4  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {wishlist.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </main>
   );
 }
