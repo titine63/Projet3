3567942c7a84fe18de186a26396b29b96bdfb7f0
Jesse Vallant <vallantjesse@live.com>
2023-10-28 03:34:38 +0200

    added a loader component

---
+++ frontend/src/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+export default function Loader() {
+  return (
+    <>
+      <div className="load-row">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </>
+  );
+}
